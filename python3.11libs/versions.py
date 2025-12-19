050b81fe3b0c46b08cceb3118e55db9f9a7b823b
timurproko <timur.proko@gmail.com>
2025-12-19 13:07:34 +0100

    removed v from versions

--- python3.11libs/versions.py
+++ python3.11libs/versions.py
@@ -36,7 +36,7 @@ def splitver(string,frmats):
 
 def foldersearch(path,matchlist,frmat,sec):
     padding = 3
-    fmt = '_v'
+    fmt = '_'
     for f in os.listdir(path[0]):
         result = splitver(f,frmat)
 
@@ -58,7 +58,7 @@ def buildpath(padding,path,matchlist,frmat,sec):
     matchlist = [int(v) for v in matchlist]
     v = str(max(matchlist)+1).zfill(padding)
 
-    return '%s/%s%s%s%s'%(path[0],sec[0],frmat,v,sec[2])
+    return '%s/%s_%s%s'%(path[0],sec[0],v,sec[2])
 
 
 
@@ -66,19 +66,19 @@ def incsave():
     
     path = hou.hipFile.path().rsplit('/',1)
     
-    sec,frmat = splitver(path[1],['_v','_V'])
+    sec,frmat = splitver(path[1],['_v','_V','_'])
     
     if sec:
         matchlist = [sec[1]]
         padding = len(sec[1])
-        matchlist = foldersearch(path,matchlist,[frmat],sec)[0]
+        matchlist = foldersearch(path,matchlist,['_v','_V','_'],sec)[0]
         
     else:
         sec = path[1].rsplit('.',1)
         sec[1]='.'+sec[1]
         sec.insert(1,'')
 
-        frmat = ['_v','_V']
+        frmat = ['_']
         
         matchlist,padding,frmat = foldersearch(path,['000'],frmat,sec)
 
